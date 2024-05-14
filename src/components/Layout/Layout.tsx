d29a0461613876a1ccbeeff5a8df61102ca023fe
SuryaVats <sunnyvats2002@gmail.com>
2024-05-14 11:07:20 +0530

    home screen layout for small size screen

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,17 @@
-import Navbar from '@/components/Layout/Navbar';
-import React from 'react';
+import Navbar from "@/components/Layout/Navbar";
+import Footer from "@/components/Layout/Footer";
+import React from "react";
 
-export default function Layout({ children }: {
-    children: React.ReactNode
-}) {
-    return (
-        <>
-            <header><Navbar /></header>
-            <main>{children}</main>
-        </>
-    )
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="md:flex ">
+      <header>
+        <Navbar />
+      </header>
+      <main>{children}</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
