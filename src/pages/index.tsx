02be84efb8af325476f24651bdc391e819c67c67
Sagar Jaswal <sagarjaswal9102001@gmail.com>
2024-05-16 11:17:55 +0530

    User Authorization Using Middleware

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,34 +7,3 @@ export default function Home() {
   </Layout>
 
 }
-
-// export async function getServerSideProps(context: any) {
-//   const req = context.req;
-//   const cookieHeader = req.headers.cookie;
-//   const authResponse = await fetch("http://localhost:8080/user/verifyAuth", {
-//     headers: {
-//       Cookie: cookieHeader,
-//     },
-//   });
-
-//   if (authResponse.status === 401) {
-//     return {
-//       redirect: {
-//         destination: "/login",
-//         permanent: false,
-//       },
-//     };
-//   }
-
-//   const authData = await authResponse.json();
-
-//   return {
-//     props: {
-//       user: {
-//         name: authData.user.name,
-//         email: authData.user.email,
-//         id: authData.user.id,
-//       },
-//     },
-//   };
-// }
